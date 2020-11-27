faab0dff58415f92d4420d3e6362ec6e34e71903
marcelraas <marcelraas@godatadriven.com>
2020-11-27 15:27:39 +0100

    Add find_packages

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name='yowo',
     version='0.0.1',
-    packages=['yowo'],
+    packages=setuptools.find_packages(),
     install_requires=[
         'pandas',
         'numpy',
